b94151699e8a2a899bcd6830bed216a969521dfb
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-30 23:38:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -36,7 +36,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect do |element, index|} 
+  array.each_with_index.collect do |element, index|
   if index != 1 
     element + "s" 
   else 
